9456163d7d5406c7e2a20738bae593f86bb7940e
Bhanu-prakash-udata <74170621+Bhanu-prakash-udata@users.noreply.github.com>
2020-11-09 11:43:59 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,36 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> print("sum",44+1)
+sum 45
+>>> print("why")
+why
+>>>  i='he'
+ 
+SyntaxError: unexpected indent
+>>> name='hi'
+>>> name
+'hi'
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> name,rollno,branch='python',2614,'cse'
+>>> name
+'python'
+>>> print(rollno)
+2614
+>>> branch
+'cse'
+>>> print('hi')
+a=10
+b=20
+c=a
+a=b
+b=c
+print("after swapping",a,b)
+
---
+++ sample1.py
@@ -0,0 +1,23 @@
+#val=50
+print("hello")
+print("today we are creating sample1 file")
+z=input("enter a string")
+print(type(z))
+b=float(input("enter a value:"))
+a=int(input("enter a value:"))
+print(b,type(b))
+print(b+a)
+a=10
+b=20
+c=a
+a=b
+b=c
+print("after swapping",a,b)
+m=10
+n=20
+m=m+n
+n=m-n
+m=m-n
+print("after swapping",m,n)
+
+
